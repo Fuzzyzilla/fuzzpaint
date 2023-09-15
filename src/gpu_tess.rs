e5cbd5a0b0c62c518b99359f76f4460a97eb7aff
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-09-14 23:32:43 -0700

    Woohoo! gpu tess implemented

--- src/gpu_tess.rs
+++ src/gpu_tess.rs
@@ -70,19 +70,204 @@ mod shaders {
 pub struct GpuStampTess {
     context: Arc<crate::render_device::RenderContext>,
     pipeline: Arc<vk::ComputePipeline>,
+    input_descriptor: Arc<vk::DescriptorSetLayout>,
+    output_descriptor: Arc<vk::DescriptorSetLayout>,
+    layout: Arc<vk::PipelineLayout>,
 }
 impl GpuStampTess {
+    fn make_layout(
+        device: Arc<vk::Device>,
+    ) -> anyhow::Result<(
+        Arc<vk::PipelineLayout>,
+        Arc<vk::DescriptorSetLayout>,
+        Arc<vk::DescriptorSetLayout>,
+    )> {
+        // Interface consists of two sets, each with a two storage buffers.
+        let buffer_binding = vulkano::descriptor_set::layout::DescriptorSetLayoutBinding {
+            descriptor_count: 1,
+            variable_descriptor_count: false,
+            stages: vulkano::shader::ShaderStages::COMPUTE,
+            ..vulkano::descriptor_set::layout::DescriptorSetLayoutBinding::descriptor_type(
+                vulkano::descriptor_set::layout::DescriptorType::StorageBuffer,
+            )
+        };
+
+        let mut input_bindings = std::collections::BTreeMap::new();
+        input_bindings.insert(0, buffer_binding.clone());
+        input_bindings.insert(1, buffer_binding.clone());
+        let output_bindings = input_bindings.clone();
+        let inputs = vulkano::descriptor_set::layout::DescriptorSetLayout::new(
+            device.clone(),
+            vulkano::descriptor_set::layout::DescriptorSetLayoutCreateInfo {
+                push_descriptor: false,
+                bindings: input_bindings,
+                ..Default::default()
+            },
+        )?;
+        let outputs = vulkano::descriptor_set::layout::DescriptorSetLayout::new(
+            device.clone(),
+            vulkano::descriptor_set::layout::DescriptorSetLayoutCreateInfo {
+                push_descriptor: false,
+                bindings: output_bindings,
+                ..Default::default()
+            },
+        )?;
+
+        Ok((
+            vk::PipelineLayout::new(
+                device,
+                vulkano::pipeline::layout::PipelineLayoutCreateInfo {
+                    push_constant_ranges: Vec::new(),
+                    set_layouts: vec![inputs.clone(), outputs.clone()],
+                    ..Default::default()
+                },
+            )?,
+            inputs,
+            outputs,
+        ))
+    }
     pub fn new(context: Arc<crate::render_device::RenderContext>) -> anyhow::Result<Self> {
         let shader = shaders::tessellate::load(context.device().clone())?;
         let entry = shader.entry_point("main").unwrap();
-        let pipeline = vk::ComputePipeline::new(
+        let (layout, input_descriptor, output_descriptor) =
+            Self::make_layout(context.device().clone())?;
+        let pipeline = vk::ComputePipeline::with_pipeline_layout(
             context.device().clone(),
             entry,
             &shaders::tessellate::SpecializationConstants::default(),
+            layout.clone(),
             None,
-            |_| (),
         )?;
 
-        todo!()
+        Ok(Self {
+            context,
+            layout,
+            pipeline,
+            input_descriptor,
+            output_descriptor,
+        })
+    }
+    /// Tessellate some strokes!
+    /// `packed_points` should contain all the stroke data back-to-back, in order.
+    /// Returns a semaphore for when the compute completes, the vertex buffer, and the draw indirection buffer.
+    pub fn tess(
+        &self,
+        strokes: &[crate::ImmutableStroke],
+        packed_points: vulkano::buffer::subbuffer::Subbuffer<[interface::InputStrokeVertex]>,
+    ) -> anyhow::Result<(
+        vulkano::sync::future::SemaphoreSignalFuture<impl vk::sync::GpuFuture>,
+        vk::Subbuffer<[interface::OutputStrokeVertex]>,
+        vk::Subbuffer<[interface::OutputStrokeInfo]>,
+    )> {
+        let mut point_index_counter = 0;
+        let mut group_index_counter = 0;
+        let mut vertex_output_index_counter = 0;
+        let input_infos = vk::Buffer::from_iter(
+            self.context.allocators().memory(),
+            vk::BufferCreateInfo {
+                usage: vk::BufferUsage::STORAGE_BUFFER | vk::BufferUsage::INDIRECT_BUFFER,
+                ..Default::default()
+            },
+            vk::AllocationCreateInfo {
+                ..Default::default()
+            },
+            strokes.iter().map(|stroke| {
+                const DENSITY: f32 = 1.0;
+
+                let num_expected_stamps = stroke
+                    .points
+                    .last()
+                    .map(|last| (last.dist / DENSITY).ceil() as u32)
+                    .unwrap_or(0);
+                let num_expected_verts = num_expected_stamps * 6;
+                let num_groups = num_expected_stamps.div_ceil(32);
+
+                let info = interface::InputStrokeInfo {
+                    start_point_idx: point_index_counter,
+                    num_points: stroke.points.len() as u32,
+                    out_vert_offset: vertex_output_index_counter,
+                    out_vert_limit: num_expected_verts,
+                    start_group: group_index_counter,
+                    num_groups,
+                    modulate: stroke.brush.color_modulate,
+                    density: DENSITY,
+                    eraser: if stroke.brush.is_eraser { 1 } else { 0 },
+                };
+
+                point_index_counter += stroke.points.len() as u32;
+                group_index_counter += num_groups;
+                vertex_output_index_counter += num_expected_verts;
+
+                info
+            }),
+        )?;
+        let output_infos = vk::Buffer::new_slice::<interface::OutputStrokeInfo>(
+            self.context.allocators().memory(),
+            vk::BufferCreateInfo {
+                usage: vk::BufferUsage::STORAGE_BUFFER | vk::BufferUsage::INDIRECT_BUFFER,
+                ..Default::default()
+            },
+            vk::AllocationCreateInfo {
+                ..Default::default()
+            },
+            strokes.len() as u64,
+        )?;
+        let output_verts = vk::Buffer::new_slice::<interface::OutputStrokeVertex>(
+            self.context.allocators().memory(),
+            vk::BufferCreateInfo {
+                usage: vk::BufferUsage::STORAGE_BUFFER | vk::BufferUsage::VERTEX_BUFFER,
+                ..Default::default()
+            },
+            vk::AllocationCreateInfo {
+                ..Default::default()
+            },
+            vertex_output_index_counter as u64,
+        )?;
+
+        let input_descriptor = vk::PersistentDescriptorSet::new(
+            self.context.allocators().descriptor_set(),
+            self.input_descriptor.clone(),
+            [
+                vk::WriteDescriptorSet::buffer(0, input_infos),
+                vk::WriteDescriptorSet::buffer(1, packed_points),
+            ],
+        )?;
+        let output_descriptor = vk::PersistentDescriptorSet::new(
+            self.context.allocators().descriptor_set(),
+            self.input_descriptor.clone(),
+            [
+                vk::WriteDescriptorSet::buffer(0, output_infos.clone()),
+                vk::WriteDescriptorSet::buffer(1, output_verts.clone()),
+            ],
+        )?;
+
+        let mut command_buffer = vk::AutoCommandBufferBuilder::primary(
+            self.context.allocators().command_buffer(),
+            self.context.queues().compute().idx(),
+            vulkano::command_buffer::CommandBufferUsage::OneTimeSubmit,
+        )?;
+
+        command_buffer
+            // Unwrap ok - Output infos is aligned to u32
+            .fill_buffer(output_infos.clone().try_cast_slice().unwrap(), 0u32)?
+            .bind_pipeline_compute(self.pipeline.clone())
+            .bind_descriptor_sets(
+                vulkano::pipeline::PipelineBindPoint::Compute,
+                self.layout.clone(),
+                0,
+                (input_descriptor, output_descriptor),
+            )
+            .dispatch([group_index_counter, 1, 1])?;
+        let command_buffer = command_buffer.build()?;
+
+        use vk::sync::GpuFuture;
+        let future = vk::sync::now(self.context.device().clone())
+            .then_execute(
+                self.context.queues().compute().queue().clone(),
+                command_buffer,
+            )?
+            .then_signal_semaphore_and_flush()?;
+
+        Ok((future, output_verts, output_infos))
     }
 }
