61a004bd9760ed050cc93f4315b95a84bc9791ae
Fuzzyzilla <fuzzyzilla01@gmail.com>
2023-09-25 12:25:24 -0700

    better lookin tool system outline :3

--- src/pen_tools.rs
+++ src/pen_tools.rs
@@ -11,7 +11,55 @@
 
 /// A trait for the visual components of tools. Completely optional!
 /// Register in [StateLayer::make_renderer]
-pub trait PenToolPreview {}
+// This will box the future. It's totally possible for this to be
+// static dispatch, but i was getting way caught up in the weeds trying to implement
+// that and there's really no need :'P
+#[async_trait::async_trait]
+trait PenTool {
+    fn new_from_renderer(
+        context: &std::sync::Arc<crate::render_device::RenderContext>,
+    ) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+    /// Process input, optionally returning a commandbuffer to be drawn.
+    async fn process(
+        &mut self,
+        stylus_input: crate::stylus_events::StylusEventFrame,
+        actions: crate::actions::ActionFrame,
+        tool_output: &mut ToolStateOutput,
+        render_output: &mut ToolRenderOutput,
+    );
+}
+
+/// Allow tools to specify their transitions at runtime, or leave None
+/// to provide default behavior.
+struct ToolStateOutput {
+    transition: Option<Transition>,
+}
+impl ToolStateOutput {
+    /// Tell the tool state to read the actions and decide for itself what tool to transition
+    /// to do, if any. Will happen regardless if no [ToolStateOutput::with_transition] is asserted.
+    pub fn with_default_behavior(&mut self) {
+        self.transition = None
+    }
+    /// Tell the tool state to perform this transition.
+    pub fn with_transition(&mut self, transition: Transition) {
+        self.transition = Some(transition)
+    }
+    /// Compute default transition for the given actions.
+    /// Does not have access to the current state on purpose, as custom
+    /// behavior per-state should be implemented in the tool itself.
+    fn do_default(actions: &crate::actions::ActionFrame) -> Transition {
+        todo!()
+    }
+}
+/// Interface for tools to (optionally) insert and read render data.
+struct ToolRenderOutput<'a> {
+    // A reference, to avoid the potentially expensive cost of cloning when the tool
+    // doesn't end up caring :P
+    render_task_messages: &'a tokio::sync::mpsc::UnboundedSender<crate::RenderMessage>,
+}
+impl ToolRenderOutput<'_> {}
 
 enum TransitionCondition {
     Pressed(crate::actions::Action),
@@ -19,26 +67,16 @@ enum TransitionCondition {
     NotHeld(crate::actions::Action),
 }
 #[derive(Copy, Clone, strum::EnumIter, Hash)]
-enum StateLayer {
+pub enum StateLayer {
     Brush,
     DocumentPan,
     DocumentScrub,
     Gizmos,
 }
-impl StateLayer {
-    // Owie boxed dyn...
-    /// Make the renderer for this type of tool.
-    pub fn make_renderer(
-        &self,
-        render_context: &std::sync::Arc<crate::render_device::RenderContext>,
-    ) -> Option<Box<dyn PenToolPreview>> {
-        match self {
-            _ => None,
-        }
-    }
-}
 enum Transition {
-    ToState(StateLayer),
+    /// Layer this state on top the base. Note that states may not modify what
+    /// state the base is!
+    ToLayer(StateLayer),
     ToBase,
     NoChange,
 }
@@ -47,6 +85,11 @@ pub struct ToolState {
     base: StateLayer,
     /// Current machine state
     layer: Option<StateLayer>,
+
+    brush: Box<dyn PenTool>,
+    document_pan: Box<dyn PenTool>,
+    document_scrub: Box<dyn PenTool>,
+    gizmos: Box<dyn PenTool>,
 }
 impl ToolState {
     /// Allow the tool to process the given stylus data and actions, optionally returning preview render commands,
@@ -56,8 +99,45 @@ impl ToolState {
         stylus_input: crate::stylus_events::StylusEventFrame,
         actions: crate::actions::ActionFrame,
     ) -> Option<std::sync::Arc<crate::vk::PrimaryAutoCommandBuffer>> {
-        let cur_state = self.layer.unwrap_or(self.base);
+        // Prepare output structs
+        let mut tool_output = ToolStateOutput { transition: None };
+        let mut render_output = ToolRenderOutput {
+            render_task_messages: todo!(),
+        };
 
-        todo!()
+        // Get current tool and run
+        let cur_state = self.get_current_state();
+        let tool = self.tool_for_state(cur_state);
+
+        tool.process(stylus_input, actions, &mut tool_output, &mut render_output)
+            .await;
+
+        // Apply output structs
+        let transition = tool_output
+            .transition
+            .unwrap_or_else(|| ToolStateOutput::do_default(&actions));
+        self.apply_state_transition(transition);
+    }
+    fn tool_for_state(&mut self, state: StateLayer) -> &mut dyn PenTool {
+        match state {
+            StateLayer::Brush => self.brush.as_mut(),
+            StateLayer::DocumentPan => self.document_pan.as_mut(),
+            StateLayer::DocumentScrub => self.document_scrub.as_mut(),
+            StateLayer::Gizmos => self.gizmos.as_mut(),
+        }
+    }
+    fn apply_state_transition(&mut self, transition: Transition) {
+        match transition {
+            Transition::NoChange => (),
+            Transition::ToBase => self.layer = None,
+            Transition::ToLayer(layer) => self.layer = Some(layer),
+        }
+    }
+    /// Set the resting state, where tools will go when no hotkey set.
+    pub fn set_base_state(&mut self, state: StateLayer) {
+        self.base = state;
+    }
+    pub fn get_current_state(&self) -> StateLayer {
+        self.layer.unwrap_or(self.base)
     }
 }
