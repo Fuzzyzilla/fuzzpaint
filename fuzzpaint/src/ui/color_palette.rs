Fuzzyzilla/fuzzpaint

commit 797fb35380f7dc491302046e19b2494152adcce7

Upgrade all dependencies  (#53)
* Upgrade compatible
* Upgrade hashbrown
* Update thiserror + rfd
* update tikv-jemallocator
* Update ultraviolet
* Update toml + strum
* Update png
* Update dirs
* update harfbuzz + fontdb + ttf_parser
* Update egui + egui-winit
* Refactor into winit Application API, remove deprecated winit calls

fuzzpaint/src/ui/color_palette.rs
@@ -97,10 +97,10 @@ impl egui::Widget for ColorSquare {
         let size = ui.style().spacing.interact_size.min_elem();
         let (rect, this) = ui.allocate_exact_size(
             egui::Vec2::splat(size),
-            egui::Sense {
-                click: enabled,
-                drag: false,
-                focusable: enabled,
+            if enabled {
+                egui::Sense::CLICK | egui::Sense::FOCUSABLE
+            } else {
+                egui::Sense::empty()
             },
         );
         this.widget_info(|| egui::WidgetInfo {
@@ -113,6 +113,7 @@ impl egui::Widget for ColorSquare {
             selected: None,
             value: None,
             text_selection: None,
+            hint_text: None,
         });
 
         // false if all in the normal range of colors
@@ -169,6 +170,7 @@ impl egui::Widget for ColorSquare {
                 color: stroke_color,
                 width: 1.0,
             },
+            egui::StrokeKind::Outside,
         );
         if let Some(icon) = self.icon {
             painter.text(
@@ -194,10 +196,10 @@ impl egui::Widget for IconSquare {
         let size = ui.style().spacing.interact_size.min_elem();
         let (rect, this) = ui.allocate_exact_size(
             egui::Vec2::splat(size),
-            egui::Sense {
-                click: enabled,
-                drag: false,
-                focusable: false,
+            if enabled {
+                egui::Sense::CLICK
+            } else {
+                egui::Sense::empty()
             },
         );
         this.widget_info(|| egui::WidgetInfo {
@@ -210,6 +212,7 @@ impl egui::Widget for IconSquare {
             selected: None,
             value: None,
             text_selection: None,
+            hint_text: None,
         });
 
         let painter = ui.painter();
@@ -361,14 +364,14 @@ impl<
     > ColorPalette<'_, '_, Writer>
 {
     pub fn show(self, ui: &mut egui::Ui) -> ColorPaletteResponse {
-        let mut changed = false;
         const BTN_BASE_SIZE: f32 = 12.0;
+        let mut changed = false;
         let width = ui.available_width();
 
         let mut response = egui::Frame::canvas(ui.style())
             .shadow(egui::epaint::Shadow::NONE)
             .stroke(egui::Stroke::NONE)
-            .rounding(0.0)
+            .corner_radius(0)
             .show(ui, |ui| {
                 egui::ScrollArea::vertical()
                     // Expand to width, dynamic height
@@ -444,11 +447,11 @@ impl<
                                     ui.separator();
                                     if ui.small_button("Pin").clicked() {
                                         new_pins.push(idx.into());
-                                        ui.close_menu();
+                                        ui.close();
                                     }
                                     if ui.small_button("Replace contents with active color").clicked() {
                                         replace_color = Some((idx, deref_color));
-                                        ui.close_menu();
+                                        ui.close();
                                     }
                                 });
 
@@ -469,7 +472,7 @@ impl<
                                 .clicked()
                             {
                                 *self.color = self.palette.insert(deref_color).into();
-                            };
+                            }
 
                             // Issue write, if requested.
                             if let Some((idx, color)) = replace_color {
@@ -502,7 +505,7 @@ impl<
                                     response.context_menu(|ui| {
                                         if ui.small_button("Unpin").clicked() {
                                             new_unpins.push(color);
-                                            ui.close_menu();
+                                            ui.close();
                                         }
                                     });
                                     if response.clicked() {
@@ -535,7 +538,7 @@ impl<
                                     response.context_menu(|ui| {
                                         if ui.small_button("Pin").clicked() {
                                             new_pins.push(color);
-                                            ui.close_menu();
+                                            ui.close();
                                         }
                                     });
                                     if response.clicked() {
@@ -580,7 +583,7 @@ impl<
             .on_hover_cursor(egui::CursorIcon::Crosshair);
 
         if changed {
-            response.mark_changed()
+            response.mark_changed();
         }
 
         ColorPaletteResponse {
@@ -601,8 +604,12 @@ pub struct PickerResponse {
 
 /// Show a collapsing color picker in the top left of the free area.
 pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> PickerResponse {
-    egui::containers::Area::new("color-picker")
-        .anchor(egui::Align2::LEFT_TOP, [0.0f32; 2])
+    egui::containers::Area::new(egui::Id::new("color-picker"))
+        .anchor(
+            egui::Align2::LEFT_TOP,
+            ctx.available_rect().left_top().to_vec2(),
+        )
+        .order(egui::Order::Background)
         .show(ctx, |ui| {
             /// number of edge "rays" from the center of the picker arc to the edge.
             const MAIN_RAYS: usize = 16;
@@ -628,7 +635,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
                 Hover,
                 SaturationValue,
                 Hue,
-            };
+            }
 
             // Remember if we were expanded last frame, make the interact area much larger.
             let click_marker = ui.id().with("click");
@@ -645,7 +652,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
             );
 
             let expanded_proportion = ctx.animate_bool(response.id, last_target.is_some());
-            let not_closed = expanded_proportion > 0.0;
+            let skip_mesh = expanded_proportion < 0.01 || ui.ctx().will_discard();
             let radius = egui::lerp(CONTRACTED_RADIUS..=EXPANDED_RADIUS, expanded_proportion);
             let origin_offset = egui::lerp(CONTRACTED_OFFSET..=0.0, expanded_proportion);
             let origin = rect.left_top() + [origin_offset; 2].into();
@@ -694,8 +701,13 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
             }
 
             let painter = ui.painter();
+            // Clip painter to within this Area. (It used to do this automatically ??)
+            let painter = painter.with_clip_rect(egui::Rect {
+                min: rect.min,
+                max: painter.clip_rect().max,
+            });
 
-            let mesh = not_closed.then(|| {
+            let mesh = (!skip_mesh).then(|| {
                 // Reserve exact amount of space. We do this all every frame so any bit of extra perf is nice.
                 // First step has a single shared vertex for all. Every ray then has STEPS more verts on it.
                 const MAIN_VERTICES: usize = 1 + (MAIN_RAYS + 1) * MAIN_STEPS;
@@ -861,13 +873,21 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
                     _ => unselected_stroke,
                 };
                 // Main picker
-                painter.circle_stroke(mesh_origin, mesh_radius, main_stroke);
+                painter.circle_stroke(
+                    mesh_origin,
+                    mesh_radius - main_stroke.width / 2.0,
+                    main_stroke,
+                );
                 // Inner for hue circle
-                painter.circle_stroke(mesh_origin, mesh_radius + RADIAL_MARGIN, hue_stroke);
+                painter.circle_stroke(
+                    mesh_origin,
+                    mesh_radius + RADIAL_MARGIN - hue_stroke.width / 2.0,
+                    hue_stroke,
+                );
                 // outer
                 painter.circle_stroke(
                     mesh_origin,
-                    mesh_radius + RADIAL_MARGIN + HUE_WIDTH,
+                    mesh_radius + RADIAL_MARGIN + HUE_WIDTH - hue_stroke.width / 2.0,
                     hue_stroke,
                 );
 
@@ -875,7 +895,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
                     mesh_origin
                         + egui::Vec2::angled(hue_angle)
                             * (mesh_radius + RADIAL_MARGIN + HUE_WIDTH / 2.0),
-                    PICKER_SIZE,
+                    PICKER_SIZE - hue_stroke.width / 2.0,
                     egui::ecolor::Hsva::new(hsva.h, 1.0, 1.0, 1.0),
                     hue_stroke,
                 );
@@ -886,7 +906,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
             // it's position on the wheel when expanded.
             painter.circle(
                 origin + expanded_proportion * egui::Vec2::angled(sv_angle) * sv_radius,
-                egui::lerp(CONTRACTED_RADIUS..=PICKER_SIZE, expanded_proportion),
+                egui::lerp(CONTRACTED_RADIUS..=PICKER_SIZE, expanded_proportion) - 0.5,
                 egui::ecolor::HsvaGamma { a: 1.0, ..*hsva },
                 egui::Stroke {
                     color: grayscale_contrasting(*hsva, egui::Rgba::WHITE),
@@ -931,6 +951,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
                             selected: None,
                             value: Some(hsva.h.into()),
                             text_selection: None,
+                            hint_text: None,
                         });
                     }
                     Some(ClickTarget::SaturationValue) => {
@@ -943,6 +964,7 @@ pub fn picker_dock(ctx: &egui::Context, hsva: &mut egui::ecolor::HsvaGamma) -> P
                             selected: None,
                             value: None,
                             text_selection: None,
+                            hint_text: None,
                         });
                     }
                     _ => (),

fuzzpaint/src/ui/drag.rs
@@ -62,11 +62,7 @@ impl DropSeparator {
             if self.active {
                 egui::Sense::hover()
             } else {
-                egui::Sense {
-                    click: false,
-                    drag: false,
-                    focusable: false,
-                }
+                egui::Sense::empty()
             },
         );
 

fuzzpaint-core/Cargo.toml
@@ -6,31 +6,31 @@ edition = "2021"
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 
 [dependencies]
-anyhow = "1.0.81"
+anyhow = "1.0.100"
 az = "1.2.1"
-base64 = {version = "0.22.0", default-features = false }
-bitflags = { version = "2.5.0", features = ["bytemuck"] }
+base64 = {version = "0.22.1", default-features = false }
+bitflags = { version = "2.10.0", features = ["bytemuck"] }
 bitvec = { version = "1.0.1", default-features = false, features = [
     "alloc",
     "std",
 ] }
-blake3 = "1.5.1"
-bytemuck = { version = "1.15.0", features = ["derive"] }
-chrono = { version = "0.4.37", default-features = false }
-crc = "3.2.1"
-dirs = "5.0.1"
-either = "1.11.0"
-fontdb = "0.16.2"
-hashbrown = { version = "0.14.3", features = ["serde"] }
+blake3 = "1.8.2"
+bytemuck = { version = "1.24.0", features = ["derive"] }
+chrono = { version = "0.4.42", default-features = false }
+crc = "3.4.0"
+dirs = "6.0.0"
+either = "1.15.0"
+fontdb = "0.23.0"
+hashbrown = { version = "0.16.1", features = ["serde"] }
 human_bytes = "0.4.3"
 id_tree = "1.8.0"
-log = "0.4.21"
-parking_lot = "0.12.1"
-rangemap = "1.5.1"
-rustybuzz = "0.13.0"
+log = "0.4.29"
+parking_lot = "0.12.5"
+rangemap = "1.7.0"
+rustybuzz = "0.20.1"
 slab_tree = "0.3.2"
-smallvec = { version = "1.13.2", features = ["serde", "union"] }
-strum = { version = "0.26.2", features = ["derive"] }
-thiserror = "1.0.58"
-unicode-segmentation = "1.11.0"
-uuid = { version = "1.8.0", features = ["v4"] }
+smallvec = { version = "1.15.1", features = ["serde", "union"] }
+strum = { version = "0.27.2", features = ["derive"] }
+thiserror = "2.0.17"
+unicode-segmentation = "1.12.0"
+uuid = { version = "1.19.0", features = ["v4"] }

fuzzpaint-core/src/repositories/fonts.rs
@@ -82,15 +82,9 @@ impl EmbedRestriction {
         let permissions = table.permissions()?;
         // These flags are meaningless unless ^^ is some, that's okay!
         let can_subset = table.is_subsetting_allowed();
-        // Misnomer - this checks if bit 9 is zero.
-        // If bit unset, this means all data embeddable, set means bitmap only.
-        let bitmap_only = !table.is_bitmap_embedding_allowed();
+        let outline_embedding_allowed = table.is_outline_embedding_allowed();
 
-        let level = if bitmap_only {
-            // Bitmap-only mode means there's no use in us embedding, at least for our purposes!
-            // (todo: for a bitmap-only font, this is actually permissive. We don't handle those yet anyway!)
-            EmbedRestrictionLevel::ReadOnly
-        } else {
+        let level = if outline_embedding_allowed {
             use rustybuzz::ttf_parser::Permissions;
             match permissions {
                 Permissions::Restricted => EmbedRestrictionLevel::LocalOnly,
@@ -98,6 +92,10 @@ impl EmbedRestriction {
                 Permissions::Editable => EmbedRestrictionLevel::Writable,
                 Permissions::Installable => EmbedRestrictionLevel::Installable,
             }
+        } else {
+            // Bitmap-only mode means there's no use in us embedding, at least for our purposes!
+            // (todo: for a bitmap-only font, this is actually permissive. We don't handle those yet anyway!)
+            EmbedRestrictionLevel::ReadOnly
         };
         Some(Self { can_subset, level })
     }

fuzzpaint-core/src/util.rs
@@ -3,7 +3,7 @@
 
 /// A float which is non-NaN
 // Because of the preconditions invalidating many bitpatterns, this is not Pod.
-#[derive(Copy, Clone, PartialEq, PartialOrd, bytemuck::NoUninit, bytemuck::Zeroable, Debug)]
+#[derive(Copy, Clone, PartialEq, bytemuck::NoUninit, bytemuck::Zeroable, Debug)]
 #[repr(transparent)]
 pub struct FiniteF32(f32);
 impl FiniteF32 {
@@ -52,12 +52,16 @@ pub enum FiniteF32Error {
 // So PartialEq can act like Eq
 impl Eq for FiniteF32 {}
 // Doing this on purpose! taking partial ord logic to impl Ord because of struct invariants.
-#[allow(clippy::derive_ord_xor_partial_ord)]
 impl Ord for FiniteF32 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         // Unwrap OK - we guarantee that the wrapped f32's are non-NaN and thus will never
         // compare as None.
-        unsafe { self.partial_cmp(other).unwrap_unchecked() }
+        unsafe { self.0.partial_cmp(&other.0).unwrap_unchecked() }
+    }
+}
+impl PartialOrd for FiniteF32 {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
 }
 impl std::hash::Hash for FiniteF32 {

fuzzpaint/Cargo.toml
@@ -7,58 +7,59 @@ edition = "2021"
 
 [dependencies]
 fuzzpaint-core = { path = "../fuzzpaint-core" }
-ahash = { version = "0.8.11", default-features = false, features = ["std"] }
-anyhow = "1.0.81"
-async-trait = "0.1.79"
+ahash = { version = "0.8.12", default-features = false, features = ["std"] }
+anyhow = "1.0.100"
+async-trait = "0.1.89"
 az = "1.2.1"
-bitflags = { version = "2.5.0", features = ["bytemuck"] }
-bytemuck = "1.15.0"
+bitflags = { version = "2.10.0", features = ["bytemuck"] }
+bytemuck = "1.24.0"
 cgmath = "0.18.0"
 crossbeam = "0.8.4"
 defer = "0.2.1"
 dhat = { version = "0.3.3", optional = true }
-dirs = "5.0.1"
+dirs = "6.0.0"
 # To enable default-font within egui-winit
-egui = "0.26.2"
-egui-winit = { version = "0.26.2", features = ["bytemuck"] }
-either = "1.10.0"
-env_logger = "0.11.3"
-hashbrown = { version = "0.14.3", features = ["serde"] }
+egui = "0.33.3"
+egui-winit = { version = "=0.33.3", features = ["bytemuck"] }
+either = "1.15.0"
+env_logger = "0.11.8"
+hashbrown = { version = "0.16.1", features = ["serde"] }
 human_bytes = "0.4.3"
-image = "0.25.0"
-log = "0.4.21"
-lyon_tessellation = "1.0.13"
+image = "0.25.9"
+log = "0.4.29"
+lyon_tessellation = "1.0.16"
 octotablet = "0.1.0"
-parking_lot = "0.12.1"
-rayon = "1.10.0"
-rfd = "0.14.1"
-rustybuzz = "0.13.0"
-serde = { version = "1.0.197", features = ["derive", "rc"] }
+parking_lot = "0.12.5"
+rayon = "1.11.0"
+rfd = "0.16.0"
+rustybuzz = "0.20.1"
+serde = { version = "1.0.228", features = ["derive", "rc"] }
 simple-logging = "2.0.2"
-smallvec = { version = "1.13.2", features = ["serde", "union"] }
-thiserror = "1.0.58"
-tokio = { version = "1.36.0", features = [
+smallvec = { version = "1.15.1", features = ["serde", "union"] }
+thiserror = "2.0.17"
+tokio = { version = "1.48.0", features = [
     "sync",
     "rt",
     "macros",
     "parking_lot",
 ] }
-toml = "0.8.12"
+toml = "0.9.10"
 try-block = "0.1.0"
-ultraviolet = { version = "0.9.2", features = ["bytemuck"] }
+ultraviolet = { version = "0.10.0", features = ["bytemuck"] }
 vulkano = { version = "0.34.0", git = "https://github.com/fuzzyzilla/vulkano.git", branch = "backport-null-check"  }
 vulkano-shaders = { version = "0.34.0", git = "https://github.com/fuzzyzilla/vulkano.git", branch = "backport-null-check"  }
 # Can't inherit from egui_winit since it doesn't enable serde feature.
 # Also need legacy RWH version for `vulkano` + new version for `octotablet`
-winit = { version = "0.29.15", features = ["serde", "rwh_05", "rwh_06"] }
+winit = { version = "0.30.12", features = ["serde", "rwh_05", "rwh_06"] }
 bus = "2.4.1"
-strum = { version = "0.26.2", features = ["derive"] }
-png = "0.17.13"
+strum = { version = "0.27.2", features = ["derive"] }
+png = "0.18.0"
 # For low-level vulkan crimes, use the same version as vulkano.
 ash = "^0.37.3"
+oneshot = "0.1.11"
 
 [target.'cfg(not(target_env = "msvc"))'.dependencies]
-tikv-jemallocator = { version = "0.5.4", optional = true, default-features = false }
+tikv-jemallocator = { version = "0.6.1", optional = true, default-features = false }
 
 [features]
 default = ["jemallocator"]

fuzzpaint/src/egui_impl.rs
@@ -37,16 +37,17 @@ impl Ctx {
         let mut renderer = Render::new(render_surface.context(), render_surface.format())?;
         renderer.gen_framebuffers(render_surface)?;
 
+        let properties = render_surface.context().physical_device().properties();
+        let max_size = properties.max_image_dimension2_d;
+
         let mut state = egui_winit::State::new(
             egui::Context::default(),
             egui::ViewportId::ROOT,
             &window,
             None,
             None,
+            Some(max_size as usize),
         );
-        let properties = render_surface.context().physical_device().properties();
-        let max_size = properties.max_image_dimension2_d;
-        state.set_max_texture_side(max_size as usize);
 
         Ok(Self {
             state,
@@ -78,7 +79,7 @@ impl Ctx {
     pub fn update<T>(
         &'_ mut self,
         window: &winit::window::Window,
-        f: impl FnOnce(&'_ egui::Context) -> T,
+        mut f: impl FnMut(&'_ egui::Context) -> T,
     ) -> T {
         let input = self.state.take_egui_input(window);
 
@@ -654,24 +655,14 @@ impl Render {
         for (_, delta) in &deltas.set {
             total_delta_size += match &delta.image {
                 egui::ImageData::Color(color) => color.width() * color.height() * 4,
-                //We'll covert to 8bpp on upload
-                egui::ImageData::Font(grey) => grey.width() * grey.height(),
             };
         }
 
-        let mut data_vec = Vec::with_capacity(total_delta_size);
+        let mut data_vec = Vec::<u8>::with_capacity(total_delta_size);
         for (_, delta) in &deltas.set {
             match &delta.image {
                 egui::ImageData::Color(data) => {
                     data_vec.extend_from_slice(bytemuck::cast_slice(&data.pixels[..]));
-                }
-                egui::ImageData::Font(data) => {
-                    //Convert f32 image to u8 unorm image
-                    data_vec.extend(
-                        data.pixels
-                            .iter()
-                            .map(|&f| (f * 255.0).clamp(0.0, 255.0) as u8),
-                    );
                 }
             }
         }
@@ -707,7 +698,6 @@ impl Render {
                 hashbrown::hash_map::Entry::Vacant(v) => {
                     let format = match delta.image {
                         egui::ImageData::Color(_) => vk::Format::R8G8B8A8_UNORM,
-                        egui::ImageData::Font(_) => vk::Format::R8_UNORM,
                     };
                     let extent = {
                         let mut extent = delta.pos.unwrap_or([0, 0]);
@@ -738,22 +728,9 @@ impl Render {
                             egui::TextureFilter::Nearest => vk::Filter::Nearest,
                         };
 
-                    let mapping = if let egui::ImageData::Font(_) = delta.image {
-                        //Font is one channel, representing percent coverage of white.
-                        vk::ComponentMapping {
-                            a: vk::ComponentSwizzle::Red,
-                            r: vk::ComponentSwizzle::One,
-                            g: vk::ComponentSwizzle::One,
-                            b: vk::ComponentSwizzle::One,
-                        }
-                    } else {
-                        vk::ComponentMapping::identity()
-                    };
-
                     let view = vk::ImageView::new(
                         image.clone(),
                         vk::ImageViewCreateInfo {
-                            component_mapping: mapping,
                             ..vk::ImageViewCreateInfo::from_image(&image)
                         },
                     )?;
@@ -792,7 +769,6 @@ impl Render {
 
             let size = match &delta.image {
                 egui::ImageData::Color(color) => color.width() * color.height() * 4,
-                egui::ImageData::Font(grey) => grey.width() * grey.height(),
             };
             let start_offset = current_base_offset as u64;
             current_base_offset += size;

commit 2f55a212738e36036aecb1b7ace4ebe7d5289980

Fix layer dnd regression from #53

fuzzpaint/src/ui/color_palette.rs
@@ -115,6 +115,9 @@ impl egui::Widget for ColorSquare {
             text_selection: None,
             hint_text: None,
         });
+        if ui.ctx().will_discard() {
+            return this;
+        }
 
         // false if all in the normal range of colors
         let out_of_gammut = self

fuzzpaint/src/ui/drag.rs
@@ -5,6 +5,9 @@ impl egui::Widget for Handle {
         let interact_height = ui.style().spacing.interact_size.y;
         let size = egui::vec2(interact_height * 2.0 / 3.0, interact_height);
         let response = ui.allocate_response(size, egui::Sense::drag());
+        if ui.ctx().will_discard() {
+            return response;
+        }
 
         // Paint six dots, a somewhat universal drag icon.
         let painter = ui.painter();

fuzzpaint/src/ui/mod.rs
@@ -1973,8 +1973,8 @@ fn graph_edit_recurse<
         let header_response = ui.horizontal(|ui| {
             let data = graph.get(id).unwrap();
 
-            // Disable everything if dragging a layer around.
-            if dnd_state.is_none() {
+            // Disable everything else if dragging a layer.
+            if dnd_state.is_some_and(|state| state.drag_target != id) {
                 ui.disable();
             }
 